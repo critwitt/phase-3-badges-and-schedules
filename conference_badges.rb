5a41416662e8670a6a3685679cdab6dd79b045d0
Chris Witte <christopher.t.witte@gmail.com>
2022-09-21 21:05:36 -0400

    commit tree c55255dd5c89683a6cdc79967d486d5cdce88788

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+def badge_maker name
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator name_array
+    name_array.map {|name| "Hello, my name is #{name}."}
+end
+
+def assign_rooms name_array
+    room = 0
+    name_array.map do |name|
+        "Hello, #{name}! You'll be assigned to room #{name_array.index(name) + 1}!"
+    end
+end
+
+def printer name_array
+    batch_badge_creator(name_array).each {|p| puts p}
+    assign_rooms(name_array).each {|p| puts p}
+end
